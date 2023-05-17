5dc5cd2ed64fcf1bebbcbe698dc7eaf59ce6e097
Shrikrishna Rai <shrikrishna.rai@itonics.de>
2023-05-17 18:03:06 +0545

    mutation added

--- src/main/java/com/example/demo/controller/BookController.java
+++ src/main/java/com/example/demo/controller/BookController.java
@@ -5,6 +5,7 @@ import com.example.demo.services.book.BookServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.graphql.data.method.annotation.SchemaMapping;
 import org.springframework.stereotype.Controller;
 
 @Controller
@@ -23,4 +24,19 @@ public class BookController {
     }
 
 
+    @SchemaMapping(typeName = "Mutation", field = "addBook")
+    public String addBook(
+            @Argument String author,
+            @Argument String name,
+            @Argument String publisher,
+            @Argument Double price) {
+        Book book = new Book(author, name, 1, publisher);
+        return null;
+    }
+
+    @SchemaMapping(typeName = "Mutation", field = "addTest")
+    public String addTest(@Argument String name) {
+        System.out.println(name);
+        return "";
+    }
 }
--- src/main/java/com/example/demo/model/Author.java
+++ src/main/java/com/example/demo/model/Author.java
@@ -2,5 +2,4 @@ package com.example.demo.model;
 
 public record Author(String id, String firstName, String lastName) {
 
-
 }
